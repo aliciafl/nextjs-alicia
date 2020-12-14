062a59cdbdf111977a4484ca00aa264d6cfad1d1
[aliciafl] <alicia.fl@outlook.com>
2020-12-14 20:32:58 +0200

    Navigation bar fixed 2

--- components/navigation.js
+++ components/navigation.js
@@ -16,9 +16,9 @@ export default function Navbar({ fixed , settings}) {
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}>
               <svg className="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-//                 <title>Menu</title>
-//                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-//            </svg>
+                 <title>Menu</title>
+                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
             </button>
           </div>
           <div
